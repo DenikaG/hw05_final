9698ba64a100357152cd2555b369ba3eaab3214e
Денис Давыдов <Davd2012@mail.ru>
2022-08-02 09:41:57 +0300

    final_changes

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -35,6 +35,11 @@ ALLOWED_HOSTS = [
     'testserver',
 ]
 
+# IP адреса, при обращении с которых будет доступен DjDT
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
 
 # Application definition
 
@@ -50,6 +55,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sorl.thumbnail',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -60,6 +66,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'yatube.urls'
